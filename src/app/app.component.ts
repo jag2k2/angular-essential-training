f7c5f4daaec0e4e8b8e8e485457cf6cff3bd4b01
jag2k2 <jag2k2@gmail.com>
2019-09-06 10:55:13 -0500

    My additions and comments from video 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'       // need component decorator on a class to make it an Angular component
 
-@Component({
-  selector: 'mw-app',
-  templateUrl: './app.component.html'
+@Component({                                    // metadata object with known properties to configure the class you are decorating.  You need AT LEAST selector and template (or template URL)
+    selector: 'mw-app',                         // The selelctor is what angular will use to locate a custom html elment and apply the component (There will be an element in index.html called app-root)
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//  Begin by creating the root module class.  We need to use a decorator (@NgModule) so Angular will know that the class code is intended to be an Angular module.  
+
+import { NgModule } from '@angular/core';   //@NgModule decorator is exported from a module included in Angular's "core-scoped" package
+import { BrowserModule } from '@angular/platform-browser';  //contains core directives and pipes for working with the DOM
 import { AppComponent } from './app.component';
 
-@NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
-})
-export class AppModule {}
+@NgModule({                             // meta-data properties: configure the object you are trying to decorate
+    imports: [                          // brings in other needed angular modules
+        BrowserModule
+    ],                        
+    declarations: [                     // makes other components, directives and pipes available to your module that don't come from another module
+        AppComponent
+    ],                   
+    bootstrap: [                        // used in the root module and lets Angular know which components(s) are starting point for the bootstrap process (entry point for app code)
+        AppComponent
+    ]                       
+})                                      // no semi because this is not a statement.  It is a piece of code that will be applied to another object
+
+export class AppModule {}               // this is our app root module.  Export is part of the module loading syntax so it can be used in other files.
